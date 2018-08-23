cd59ca7ffe2a34eea2adb456ab975d3a0482de8c
wo <wojing000@gmail.com>
2018-08-23 14:15:31 +0800

    增加去重类dupliterDetailPage

---
+++ dyttCrawl/dyttCrawl/dupliterDetailpage.py
@@ -0,0 +1,20 @@
+import os
+
+from scrapy.dupefilters import RFPDupeFilter
+from scrapy.utils.request import request_fingerprint
+
+class CustomFilter(RFPDupeFilter):
+    """A dupe filter that considers specific ids in the url"""
+    def isListPage(self,url):
+        if "list" in url or "index" in url:
+            return False
+
+    def request_seen(self, request):
+        url = request.url
+        if self.isListPage(url):
+            return False
+        elif url in self.fingerprints:
+            return True
+        self.fingerprints.add(url)
+        if self.file:
+            self.file.write(url + os.linesep)
