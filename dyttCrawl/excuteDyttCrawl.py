8426819add259b5a7b7d3b95e1aa605161b68cf1
刘沃劲 <gzliuwojin@iwincaipiao.com>
2018-08-24 21:16:13 +0800

    增加执行文件

--- dyttCrawl/excuteDyttCrawl.py
+++ dyttCrawl/excuteDyttCrawl.py
@@ -4,4 +4,4 @@ import platform
 if platform.system() == "Windowns":
     cmdline.execute("scrapy crawl dytt -s JOBDIR=/log".split())
 else:
-    cmdline.execute("scrapy crawl dytt -s JOBDIR=./log".split()
+    cmdline.execute("scrapy crawl dytt -s JOBDIR=./log".split())
